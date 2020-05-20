7f42f30d0d0511f50f226981796afce55b0d8a40
Ivan Ermolenko <iermolenko@kameleoon.com>
2020-05-20 21:17:13 +0300

    Setting the registration form - input and buttons componnets were added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import {Switch,Route} from 'react-router-dom';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sig-in-and-sign-up/sig-in-and-sign-up.component';
+
 import './App.css';
 
 function App() { 
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
