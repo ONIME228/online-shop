9402b4c8734f4920d34731a4db113f406c521b03
Ivan Ermolenko <iermolenko@kameleoon.com>
2020-08-23 11:17:11 +0300

    moving out directory and shop data into its own reducer,updating components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title,imageUrl,size}) => (
+const MenuItem = ({title,imageUrl,size,linkUrl}) => (
 
-  <div className={`${size} menu-item`}>
+  <div className={`${size?size:''} menu-item`}>
     <div className="background-image"
       style={{
         backgroundImage:`url(${imageUrl})`
     }} 
     />
-    <div className="content">
+    <div className="content" onClick={()=>window.location.pathname = linkUrl}>
       <h1 className="title"> {title.toUpperCase()} </h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
