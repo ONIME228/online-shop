95595c7ca41dacf38d7d0696be2bdc8a6cc125e9
Ivan Ermolenko <iermolenko@kameleoon.com>
2020-11-25 21:46:58 +0300

    start optimizing app

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,25 +1,32 @@
-import React,{useEffect} from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
-import CollectionOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+// import CollectionOverviewContainer from '../../components/collections-overview/collections-overview.container';
+// import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
 
-const ShopPage = ({fetchCollectionsStart,match}) => {
-    useEffect(()=>{
+const CollectionOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
+
+const ShopPage = ({ fetchCollectionsStart, match }) => { 
+    useEffect(() => {
         fetchCollectionsStart();
-    },[fetchCollectionsStart]);
-        return (
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionOverviewContainer}/>
-                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />  
-            </div>
-        );
+    }, [fetchCollectionsStart]);
+    return (
+        <div className='shop-page'>
+            <Suspense fallback={<Spinner/>}>
+                <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+            </Suspense>
+
+        </div>
+    );
 };
 
 
 const mapDispatchToProps = dispatch => ({
-   fetchCollectionsStart:() => dispatch(fetchCollectionsStart()),
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 
